43d7965bde43f8fe755447071e90b1938cdd5fef
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-02-16 19:55:44 -0300

    completo

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,32 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numerouno;
 	
+	numerouno=Math.floor((Math.random() * 10) + 1);
+	console.log(numerouno);
+	if(numerouno >= 9)
+	{
+		alert(" excelente ");
+	}
+	else if(numerouno >= 4 && numerouno < 9)
+	{
+		alert( " aprobo ");
+	}
+	else
+	{
+		alert( " Vamos, la proxima se puede ");
+	}
 
-}//FIN DE LA FUNCIÓN
+/*
+	if ( numerouno >= 9 )  {
+
+	} else  { 
+		if ( numerouno >= 4 && numerouno < 9 ) {
+
+		} else {
+
+		}
+	} 
+*/
+}//FIN DE LA FUN
