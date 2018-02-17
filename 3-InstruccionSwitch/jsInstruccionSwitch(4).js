e43e4c420645eda15904b6bcc68a2aa2e274ebe0
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-02-16 22:00:16 -0300

    ejercicio4bien

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,7 +5,31 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch(mesDelAño)
+
+	{
+		case "Febrero":
+
+				alert("tiene 28 dias");
+				break;
+
+		case "Enero":
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Octubre":
+		case "Noviembre":
+		case "Diciembre":
+
+				alert("tienen 31 dias");
+				break;
+
+		default:
+
+				alert("tienen 30 dias");
+				break;
+
+	}
 
 
 
