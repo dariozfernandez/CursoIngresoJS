e2df1bc64d7430965a96d66ce00a859094518c1f
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-02-16 21:10:19 -0300

    mal hecho

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,36 @@
 function Mostrar()
 {
+var numerouno;
+var numerodos;
+var resultado;
+
+numerouno=prompt().value;
+numerodos=prompt().value;
+
+
+numerouno=parseint(numerouno);
+numerodos=parseint(numerodos);
+
+
+if( numerouno == numerodos)
+
+	{
+	resultado=numerouno*numerodos;
+	}
+	else{
+
+		if(numerouno > numerodos)
+
+		{
+			resultado=numerouno-numerodos;
+		}
+		}
+		else
+			{
+			resultado=numerouno+numerodos;
+			}
+		}
+
 
 }
+
