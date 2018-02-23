02524b403a1c6fb745317006d55e9d5aa4ac54c8
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-02-23 20:00:49 -0300

    ejercicio9 ok

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,15 +1,37 @@
 function Mostrar()
 {
-
-	var contador=0;
-	// declarar variables
-	
+		//bandera
+	var contador=0 , numero;	// declarar variables
+	var maximo;
+	var minimo;
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
-		
-	
+		numero=prompt(numero);
+		numero=parseint(numero);
+
+		while(isNaN(numero));
+			numero=prompt();
+			numero=parseint();
+
+		if ( contador == 0)//bandera
+		{
+			contador++ //bandera
+			numero=numero;
+			numero=minimo;
+
+		}
+		else
+		{
+			if (maximo < numero)
+			{
+				maximo=numero;
+			}
+			else if (minimo>numero)
+
+				minimo=numero;
+		}
 	}
 
 
