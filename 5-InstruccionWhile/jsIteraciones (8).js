9a71c15b081aaadc1b70339f1320ec4805eadc43
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-02-23 19:48:39 -0300

    ejercicio8 ??

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -7,6 +7,26 @@ function Mostrar()
 	
 	var respuesta='si';
 
+	while(resuesta== "si")
+	{
+		numero=prompt();
+		numero=parseint(numero);
+		while(isNaN(num))
+		{
+			numero=prompt();
+			numero=parseint(numero);
+		}
+
+		if(numero>0)
+		{
+			positivo+=numero;
+		}
+		else if (num<0)
+		{
+			negativo*=numero;
+		}
+		resuesta=prompt();
+	}
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
