068c2be857e1d8af68b9132c39bb1b2261e736b1
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-02-23 21:41:19 -0300

    ejercicio1 bien

---
+++ 6-InstruccionFor/jsIteraciones (1).js
@@ -0,0 +1,16 @@
+function Mostrar()
+{
+	alert('iteración while');
+
+	var contador;
+	contador=0;
+
+	while(contador<10)
+	{
+		contador++;
+		document.write("<br>" + contador);
+	}
+
+		document.write( "<br>" + contador);
+		
+}//FIN DE LA FUNCIÓN
