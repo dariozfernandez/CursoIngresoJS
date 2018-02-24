98144bc270a7fb434072974646fcbe71a616179e
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-02-23 21:41:39 -0300

    ejercicio2 bien

---
+++ jsIteraciones (2).js
@@ -0,0 +1,18 @@
+function Mostrar()
+{
+	alert('iteración while');
+
+	var contador;
+	contador = 11;
+
+	while( contador > 0)
+	{
+		contador--;
+		document.write("<br>"  + contador);
+
+
+	}
+
+		document.write("<br> sali" + contador);
+
+}//FIN DE LA FUNCIÓN
