33581d8af0314f5c0b19a8cd70ebb00d4809a84f
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-02-25 16:40:31 -0300

    ejercicio7 bien

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -5,6 +5,26 @@ function Mostrar()
 	var acumulador=0;
 	var respuesta='si';
 
+	while(respuesta=="si")
+
+		{
+			contador++;// suma ilimitada
+			numero= prompt("ingrese un numero");
+			numero= parseInt(numero);//me dice que el numero es un entero
+			
+		}
+
+		while(isNaN(numero))
+
+		{
+
+			numero=prompt(" ingrese un numero");
+			numero=parseInt(numero);
+			
+		}
+
+		acumulador= acumulador + numero;
+		respuesta=prompt(" para continuar ingrese si");
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
