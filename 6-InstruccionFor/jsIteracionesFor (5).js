de592a7db53ae095a3f105f5c10854a3e2a0f9f8
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-02-25 17:44:28 -0300

    este es el 5 mal

--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -1,5 +1,27 @@
 function Mostrar()
 {
+		
+		i=prompt("ingrese el numero");
+
+	for (var i=0; i<9 ; i++)
+
+		{
+			if (i != 9)
+
+				{
+						i=prompt("ingrese el numero nuevamente");
+				
+
+				}
+
+					else (i=9)
+					{
+
+						break;
+					}
+		}
+
+		
 
 
 
