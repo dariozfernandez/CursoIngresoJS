b1c0c2b40362952c89ec74e892b44c485f1a4b75
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-02-24 21:47:59 -0300

    ejercicio5 bien

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -3,6 +3,17 @@ function Mostrar()
 
 var sexo = prompt("ingrese f ó m .");
 
+	while(sexo=="f")
+	{
+		sexo= prompt (" el sexo es femenino");
+
+	}
+		while (sexo=="m")
+		{
+			sexo= prompt(" el sexo es masculino");
+	
+		}
+	sexo= prompt(" error : lo ingresado es incorrecto");
 
 
 document.getElementById('Sexo').value=sexo;
