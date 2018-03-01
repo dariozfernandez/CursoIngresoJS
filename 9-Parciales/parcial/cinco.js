37581720b881291765bc409c0f3799cf5ecbc544
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-03-01 20:56:21 -0300

    parcial5 bien

--- 9-Parciales/parcial/cinco.js
+++ 9-Parciales/parcial/cinco.js
@@ -1,4 +1,39 @@
 function Mostrar()
 {
+		//al ingresar 3 numeros, decir cual es el mayor.
 
+
+	var numeroUno;
+	var numeroDos;
+	var numeroTres;
+	var mayor;
+	
+
+	numeroUno= prompt("ingrese un numero");
+	numeroUno= parseInt(numeroUno);
+	numeroDos= prompt("ingrese un segundo numero");
+	numeroDos= parseInt(numeroDos);
+	numeroTres=prompt("ingrese un tercer numero");
+	numeroTres=parseInt(numeroTres);
+
+	if (numeroUno > numeroDos && numeroUno > numeroTres)
+
+	{
+				mayor = numeroUno;
+	}
+	else
+	{
+		if (numeroDos > numeroUno && numeroDos > numeroTres)
+
+				{
+					mayor = numeroDos;
+				}
+				else	
+				{
+					mayor = numeroTres;
+				}
+	}
+
+			document.write("El mayor es: " + mayor)
 }
+
