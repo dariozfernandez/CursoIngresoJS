8374730d82f17c4097d4c7e0628728a1d509c80d
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-03-01 20:57:11 -0300

    parcial 1 ??

---
+++ uno.js
@@ -0,0 +1,28 @@
+
+function Mostrar()
+{
+		//pedir la altura de un triangulo equilatero. informar el perimetro y la superficie
+
+		var laBase= 0;
+		var altura= 0;
+		var perimetro= 0;
+		var superficie= 0 ;
+
+
+		laBase=document.getElementById('laBase').value;
+		laBase=parseInt(laBase);
+
+		altura=prompt('ingrese la altura').value;
+		altura=parseInt(altura);
+
+		superficie= (laBase * altura)/2;
+
+
+
+
+
+
+
+
+
+}
