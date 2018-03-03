6a5b1da41aacce414832ea6f28abb7099eea7465
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-03-02 21:52:58 -0300

    parcial 2 ejercicio 4 bien

--- 9-Parciales/parcial2/4-if.js
+++ 9-Parciales/parcial2/4-if.js
@@ -1,7 +1,45 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 4-if");
+
+	//Realizar el algoritmo que al presionar el botón "Mostrar" pida dos números por prompt, si son iguales que
+	//los multiplique, si el primero es mayor al segundo que los reste y si no que los sume, mostrar el resultado 
+	//por document.write.
+
 	
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+ 	numeroUno= prompt("ingrese un numero");
+ 	numeroUno= parseInt(numeroUno);
+
+ 	numeroDos= prompt("ingrese un numero");
+ 	numeroDos= parseInt(numeroDos);
+
+	if (numeroUno == numeroDos)
+	
+	{
+
+			resultado = numeroUno + "" +numeroDos;
+
+			}
+				else
+				{
+
+					if(numeroUno > numeroDos)
+					{
+
+					resultado= numeroUno - numeroDos;
+
+					}
+					else
+					{
+
+						resultado= numeroUno + numeroDos;
+					}
+				}	
+
+		document.write("<br> el resultado es " + resultado);
 }
 
