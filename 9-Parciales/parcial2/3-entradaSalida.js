4f401f51749a7d96279da5be98751b07a1983988
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-03-02 21:54:19 -0300

    parcial 2 ejercicio 3 incompleto

--- 9-Parciales/parcial2/3-entradaSalida.js
+++ 9-Parciales/parcial2/3-entradaSalida.js
@@ -1,7 +1,25 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 3-EntradaSalida");
 	
+	
+	//Realizar el algoritmo que al presionar el botón "Mostrar" tome por ID dos datos,
+	// el largo y ancho de un terreno y luego que muestre cuantos metros de alambre se necesitan para
+	// colocarle 6 hilos de alambrado a su perímetro
+
+	var largo;
+	var ancho;
+	var perimetro;
+
+	largo= document.getElementById('largo').value;
+	ancho= document.getElementById('ancho').value;
+
+	largo= parseInt(largo);
+	ancho= parseInt(ancho);
+
+	perimetro= (largo * 2 + ancho * 2) * 6;
+
+	alert(" se necesitaron " + perimetro " metros de alambre");
+
 }
 
