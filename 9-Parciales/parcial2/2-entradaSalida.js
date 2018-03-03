aadb1ceeb68a3aeed42f386683235673926b16bd
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-03-02 21:48:18 -0300

    parcial 2 ejercicio 2 bien

--- 9-Parciales/parcial2/2-entradaSalida.js
+++ 9-Parciales/parcial2/2-entradaSalida.js
@@ -1,7 +1,20 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
+
+//Realizar el algoritmo que al presionar el botón "Mostrar" lea un importe de un producto por prompt y muestre el importe final
+// sumándole el IVA (21%)function Mostrar()
 {
-	alert("Funciona 2-EntradaSalida");
+  
+  var importe;
+  var importeFinal;
+  var iva;
+
+  importe= prompt("ingrese el importe");
+  importe= parseInt(importe);
+
+  importeFinal= importe + (importe * 21 / 100);
+
+  alert("su importe final es " + importeFinal);
 	
 }
 
