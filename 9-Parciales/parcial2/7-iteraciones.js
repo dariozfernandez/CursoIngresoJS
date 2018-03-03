639d96fb61d1a1d8134c25c6150dc16d9e0e7df4
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-03-02 22:16:48 -0300

    parcial 2 ejercicio 7 incompleto

--- 9-Parciales/parcial2/7-iteraciones.js
+++ 9-Parciales/parcial2/7-iteraciones.js
@@ -2,6 +2,25 @@
 function Mostrar()
 {
 	alert("Funciona 7-iteraciones");
+
+	var nota;
+	var sexo;
+	var acumulador;
+
+	for(i=0 ; i<0 ; i++) {
+
+		while(sexo != "f" && sexo != "m")
+		{
+			sexo=prompt("");
+		}
+
+		while (nota<0 || nota > 10)
+		{
+			nota= prompt("");
+			nota=parseInt(nota);
+		}
+		acum += nota;
+	}
 	
 }
 
