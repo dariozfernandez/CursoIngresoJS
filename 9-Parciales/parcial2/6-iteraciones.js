926b16b4e4bc504e15021e272d7447bb01b3e492
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-03-07 14:13:18 -0300

    parcial 2 ejercicio 6 bien

--- 9-Parciales/parcial2/6-iteraciones.js
+++ 9-Parciales/parcial2/6-iteraciones.js
@@ -1,62 +1,51 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 6-iteraciones");
-
-	//Realizar el algoritmo que al presionar el botón "Mostrar" pida el importe de las ventas 
-	//(validar que sea mayor a 0,”cero”) de los 7 días de la semana por prompt (una por día), e informar 
-	//cual fue el mayor importe y cuál fue el menor importe de venta
-
-	var importe;
-	var max;
-	var min;
-	var contador;
-	var diaMax;
-	var diaMin;
+	//Realizar el algoritmo que al presionar el botón "Mostrar" pida el importe de las ventas (validar que sea mayor a 0,”cero”)
+	 //de los 7 días de la semana por prompt (una por día), e informar cual fue el mayor importe y cuál fue el menor importe de venta
 
 
-	
+	 var importe;
+	 var contador=0;
+	 var importeMayor;
+	 var importeMenor;
 
-	while( contador<7)
-		{
+	 while (contador < 7)
 
-		contador++;
-		importe= prompt("ingrese el importe");
-		importe= parseInt(importe);
+	 {
+	 	contador ++;
+	 	importe= parseInt(prompt("ingrese el importe"));
 
-		}
-		while( importe <= 0 || isNan(importe))
-			{
+	 	while (isNaN(importe) || importe <= 0)
+	 		 {
 
-			contador++
-			importe= prompt("ingrese el importe");
-			importe= parseInt(importe);
+	 		 	importe=prompt( "error: ingrese el importe nuevamente");
 
-			}	
-		if(contador== 1)
-		{
-			diaMax= contador;
-			max= importe;
-			min= importe;
-			diaMin= contador;
-		}
+	 		 }
+	 	if (contador == 1)
+	 	{
+	 		importeMayor = importe;
+	 		importeMenor = importe;
 
-			else
-			{
+	 	}
+	 		else
+	 			{
+	 				if (importeMayor < importe)
 
-				if( importe> max)
-				{
-					diaMax= contador;
-					max= importe;
-				}
+	 				{
+	 					importeMayor = importe;
+	 				}
 
-				if( importe< min)
-				{
-					diaMin=contador;
-					min= importe;
-				}
-			}
+	 				if (importeMenor > importe)
 
+	 				{
+	 					importeMenor = importe;
+	 				}
+	 			}
+
+
+
+	 }
+	document.write("el importe mayor es" + importeMayor + " y el importe menor es " + importeMenor);
 }
-
 
