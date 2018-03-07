1e2e55d712425481a742119f3240c6e97e629bbf
dariozfernandez <36281510+dariozfernandez@users.noreply.github.com>
2018-03-07 14:14:09 -0300

    parcial 2 ejercicio 7 bien

--- 9-Parciales/parcial2/7-iteraciones.js
+++ 9-Parciales/parcial2/7-iteraciones.js
@@ -1,26 +1,69 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 7-iteraciones");
+	//Realizar el algoritmo que al presionar el botón "Mostrar" pida por prompt las notas (validar entre 0 y 10)
+	// y el sexo(validar el sexo “f” o “m”) de 6 alumnos, informar por alert: 
+//a) el promedio de las notas totales. 
+//b) la nota más baja. 
+//c) la cantidad de varones que su nota haya sido mayor o igual a 6.
 
-	var nota;
-	var sexo;
-	var acumulador;
+var nota;
+var sexo;
+var contador=0;
+var notaMasBaja;
+var sumaNotas=0;
+var promedioNotas;
+var notaMayorVarones=0;
 
-	for(i=0 ; i<0 ; i++) {
 
-		while(sexo != "f" && sexo != "m")
-		{
-			sexo=prompt("");
-		}
+while (contador < 6)
 
-		while (nota<0 || nota > 10)
-		{
-			nota= prompt("");
-			nota=parseInt(nota);
-		}
-		acum += nota;
-	}
+	{
+		contador ++;
+		nota= parseInt(prompt("ingrese la nota aqui"));
+		sexo= prompt(" ingrese el sexo del alumno");
 	
+		while(isNaN(nota) || nota > 10 || nota <= 0 )
+
+			{
+				nota=prompt("error ingrese la nota nuevamente");
+			}
+
+		while (sexo != "f" && sexo != "m")
+			
+
+			{
+				sexo= prompt(" error ingrese el sexo nuevamente");
+			}
+
+			if( contador == 1)
+				{
+					notaMasBaja = nota;
+				}
+
+			else
+				{
+					if (notaMasBaja > nota)
+
+					{
+						notaMasBaja = nota;
+					}
+
+				}
+
+			if(sexo == "m" && nota >= 6)
+			
+				{
+					notaMayorVarones++;
+				}	
+
+			sumaNotas= nota + sumaNotas;
+	}
+
+	promedioNotas= sumaNotas / contador;
+
+	document.write("<br> el promedio de las notas totales es " + promedioNotas + " , la nora mas baja es " + notaMasBaja + " y la cantidad de varones que la nota fue mayor igual a 6 es" + notaMayorVarones);
 }
+
+
 
